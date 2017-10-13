ec21fd83d17a94f047a36dba2055de13efc85aaa
muhammedalys <muhammed_aly_ozen@hotmail.com>
2017-10-13 05:35:17 -0700

    Add files via upload

---
+++ LICENSE.md/sketch_oct13b.ino
@@ -0,0 +1,30 @@
+#include <IRremote.h>
+
+IRrecv irrecv(11);
+
+decode_results results; 
+
+
+#define BUTON0 65
+
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  pinMode(3, OUTPUT);
+  digitalWrite(3, LOW);
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results))
+  {
+    Serial.println(results.value);
+    if(results.value == BUTON0)
+    digitalWrite( 3, !digitalRead(3));
+    irrecv.resume();
+  }
+  
+}
